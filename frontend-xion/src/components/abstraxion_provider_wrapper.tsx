8a43a7714c83d4d209730d554027e75cffb59698
akashSingh <singhsahab2k3@gmail.com>
2025-04-04 21:34:04 +0530

    abstraction done

--- frontend-xion/src/components/abstraxion_provider_wrapper.tsx
+++ frontend-xion/src/components/abstraxion_provider_wrapper.tsx
@@ -1,7 +1,5 @@
 "use client";
 import { AbstraxionProvider } from "@burnt-labs/abstraxion";
-// import "@burnt-labs/abstraxion/dist/index.css";
-// import "@burnt-labs/abstraxion/dist/index.css";
 import React from "react";
 
 const AbstraxionProviderWrapper = ({
@@ -11,8 +9,8 @@ const AbstraxionProviderWrapper = ({
 }) => {
   const treasuryConfig = {
     treasury: process.env.NEXT_PUBLIC_TREASURY_CONTRACT,
-    rpcUrl: "https://rpc.xion-testnet-2.burnt.com:443",
-    restUrl: "https://api.xion-testnet-2.burnt.com:443",
+    rpcUrl: "https://rpc.xion-testnet-2.burnt.com/",
+    restUrl: "https://api.xion-testnet-2.burnt.com/"
   };
 
   return (
