c6c853855e0e9d1d341c4d8d278878cbe609b5f1
Shiv Raj <shiv@Shivs-MacBook-Air.local>
2025-04-04 20:58:05 +0530

    Re-added frontend-xion as a normal folder

---
+++ frontend-xion/src/components/abstraxion_provider_wrapper.tsx
@@ -0,0 +1,23 @@
+"use client";
+import { AbstraxionProvider } from "@burnt-labs/abstraxion";
+// import "@burnt-labs/abstraxion/dist/index.css";
+// import "@burnt-labs/abstraxion/dist/index.css";
+import React from "react";
+
+const AbstraxionProviderWrapper = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  const treasuryConfig = {
+    treasury: process.env.NEXT_PUBLIC_TREASURY_CONTRACT,
+    rpcUrl: "https://rpc.xion-testnet-2.burnt.com:443",
+    restUrl: "https://api.xion-testnet-2.burnt.com:443",
+  };
+
+  return (
+    <AbstraxionProvider config={treasuryConfig}>{children}</AbstraxionProvider>
+  );
+};
+
+export default AbstraxionProviderWrapper;
